2ac3a67299e21d81cdfbb8cffb45b23b0fbb2284
borzois <borzois@h.h>
2022-04-25 12:09:10 +0300

    refactoring

--- 4chan-dl.py
+++ 4chan-dl.py
@@ -9,6 +9,35 @@ import sys
 
 # whatever im gonna make it a 4chan scraper now
 
+def download_post(post, dl_dir):
+	img = post.find('img')
+	if img is None:
+		return False
+
+	img_name = post.find(class_="fileText").a.text
+	img_link = "http:" + post.find(class_="fileThumb").get('href')
+
+	# archived posts will have a gif that messes everything up
+	if img_link.split('/')[-1] == "archived.gif":
+		return False
+
+	# download:
+	file_location = dl_dir + "/" + img_name
+	if not exists(file_location):
+		img_get = requests.get(img_link)
+		if img_get.ok:
+			with open(file_location, 'wb') as file:
+				file.write(img_get.content)
+			return True
+	else:
+		return False
+
+def print_stats(dl_count, skip_count, dl_dir):
+	if dl_count != 0:
+		print("Downloaded", dl_count, "images to", dl_dir, "\nSkipped", skip_count, "images")
+	else:
+		print("Nothing to download")
+
 def main():
 	if len(sys.argv) == 1:
 		url = input("Thread: ")
@@ -29,32 +58,16 @@ def main():
 	skip_count = 0
 
 	for post in soup.find_all(class_='postContainer'):
-		post_id = post.get('id')[2:]
-		img = post.find('img')
-		if img is not None:
-			img_name = post.find(class_="fileText").a.text
-			img_link = "http:" + post.find(class_="fileThumb").get('href')
+		download_successful = download_post(post, dl_dir)
 
-			# archived posts will have a gif that fucks everything up
-			if img_link.split('/')[-1] != "archived.gif":
-				# download:
-				file_location = dl_dir + "/" + img_name
-				if not exists(file_location):
-					print('Downloading post', post_id, '-', img_name)
-					img_get = requests.get(img_link)
-					if img_get.ok:
-						with open(file_location, 'wb') as file:
-							file.write(img_get.content)
-						dl_count += 1
-				else:
-					# skips images that have already been downloaded
-					print('Skipping post', post_id, '-', img_name)
-					skip_count += 1
+		if download_successful:
+			print('Downloaded post', post.get('id')[2:], '-', post.find(class_="fileText").a.text)
+			dl_count += 1
+		else:
+			print('Skipped post', post.get('id')[2:], '-', post.find(class_="fileText").a.text)
+			skip_count += 1
 	
-	if dl_count != 0:
-		print("Downloaded", dl_count, "images to", dl_dir, "\nSkipped", skip_count, "images")
-	else:
-		print("Nothing to download")
-
+	print_stats(dl_count, skip_count, dl_dir)
+	
 if __name__ == "__main__":
 	main()
